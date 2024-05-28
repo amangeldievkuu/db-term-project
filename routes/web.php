0e1843c89eb718b1f621016dbb630bf8c58d02a8
amangeldievkuu <amangeldievkuu@gmail.com>
2024-05-28 22:37:24 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+    return view('jobs', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/{jobId}', function ($jobId) {
+    $job = Job::find($jobId);
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
