16f0ce8543e0fc60f159e3f949f1328c35ec5780
amangeldievkuu <amangeldievkuu@gmail.com>
2024-05-29 23:46:47 +0700

    custom button, create job

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,33 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(3);
-    return view('jobs', ['jobs' => $jobs]);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{jobId}', function ($jobId) {
     $job = Job::find($jobId);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    //validations...
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
