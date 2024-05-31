f1d8950b668436aeb90a4342f6d9a9e2fcb6bad7
amangeldievkuu <amangeldievkuu@gmail.com>
2024-05-31 12:42:51 +0700

    route update

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate(3);
+        return view('jobs.index', [
+            'jobs' => $jobs,
+        ]);
+    }
+
+    //create
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    //show
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    //store
+    public function store()
+    {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    //edit
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    //update
+    public function update(Job $job)
+    {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required'],
+        ]);
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+
+        return redirect('/jobs/' . $job->id);
+    }
+
+    //destroy
+    public function destroy(Job $job)
+    {
+        $job->delete();
+        return redirect('/jobs');
+    }
 }
