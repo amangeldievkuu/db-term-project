2bd6733829560904d0b36fbe364984af33465e47
amangeldievkuu <amangeldievkuu@gmail.com>
2024-06-01 10:27:08 +0700

    update employer factory

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -14,7 +15,20 @@ class SessionController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        $validated = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($validated)) {
+            throw ValidationException::withMessages([
+                'email' => 'sorry, those credentials do not match',
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/jobs');
     }
 
     public function destroy()
