68329f8bb422c66d90c359d6eec13753f67b04d9
amangeldievkuu <amangeldievkuu@gmail.com>
2024-06-01 15:38:43 +0700

    mail, policy, queue sending email

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,25 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    $job = Job::first();
+    TranslateJob::dispatch($job);
+    return 'done';
+});
 
 Route::controller(JobController::class)->group(function () {
     Route::view('/', 'home');
-    Route::get('/jobs', 'index');               //Index 
-    Route::get('/jobs/create', 'create');       //Create
-    Route::get('/jobs/{job}', 'show');          //Show
-    Route::post('/jobs', 'store');              //Store
-    Route::get('/jobs/{job}/edit', 'edit');     //Edit
-    Route::patch('/jobs/{job}', 'update');      //Update
-    Route::delete('/jobs/{job}', 'destroy');    //Delete
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store')->middleware('auth');
+    Route::get('/jobs/{job}/edit', 'edit')->middleware('auth')->can('edit', 'job');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
 });
 
 
@@ -22,7 +30,7 @@ Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 //Login
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 
 //Logout
